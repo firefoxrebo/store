eda6dd7ec6de7b98252fe972e7bf9c75b38046da
Mohammed Yehia <m.yehia@dev-gate.com>
2017-01-02 12:14:56 +0300

    modified the menus

--- app/template/sidebar.tpl.php
+++ app/template/sidebar.tpl.php
@@ -3,31 +3,61 @@
 		<div class="box">
             <?php $userPrivilege = $this->_registry->session->u->privilege; ?>
             <ul class="mainNav">
-                <li><a href="/moneyin" class="<?php if($this->highlightMenu('moneyin')) echo 'selected'; ?>"><i class="material-icons">move_to_inbox</i></a></li>
-                <li><a href="/moneyout" class="<?php if($this->highlightMenu('moneyout')) echo 'selected'; ?>"><i class="fa fa-gift"></i></a></li>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/volunteers" class="<?php if($this->highlightMenu('volunteers')) echo 'selected'; ?>"><i class="material-icons">group</i></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                <li><a href="/client" class="<?php if($this->highlightMenu('client')) echo 'selected'; ?>"><i class="material-icons">contacts</i></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/store" class="<?php if($this->highlightMenu('store')) echo 'selected'; ?>"><i class="material-icons">store</i></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/fillstore" class="<?php if($this->highlightMenu('fillstore')) echo 'selected'; ?>"><i class="material-icons">store</i><i class="fa fa-arrow-down"></i></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1))) { ?>
-                <li><a href="/employee" class="<?php if($this->highlightMenu('employee')) echo 'selected'; ?>"><i class="fa fa-user"></i></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/expenses" class="<?php if($this->highlightMenu('expenses')) echo 'selected'; ?>"><i class="fa fa-money"></i></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1))) { ?>
-                    <li><a href="/report" class="<?php if($this->highlightMenu('report')) echo 'selected'; ?>"><i class="fa fa-bar-chart"></i></a></li>
-                <?php } ?>
-                <li><a href="/mail" class="<?php if($this->highlightMenu('mail')) echo 'selected'; ?>"><i class="fa fa-envelope"></i></a></li>
-                <li><a href="/notification" class="<?php if($this->highlightMenu('notification')) echo 'selected'; ?>"><i class="fa fa-bell"></i></a></li>
+                <li>
+                    <a href="/suppliers" class="<?php if($this->highlightMenu('suppliers')) echo 'selected'; ?>">
+                        <i class="material-icons">group</i>
+                    </a>
+                    <span><?= $text_ctrl_suppliers; ?></span>
+                </li>
+
+                <li>
+                    <a href="/clients" class="<?php if($this->highlightMenu('clients')) echo 'selected'; ?>">
+                        <i class="material-icons">contacts</i>
+                    </a>
+                    <span><?= $text_ctrl_clients; ?></span>
+                </li>
+
+                <li>
+                    <a href="/store" class="<?php if($this->highlightMenu('store')) echo 'selected'; ?>">
+                        <i class="material-icons">store</i>
+                    </a>
+                    <span><?= $text_ctrl_store; ?></span>
+                </li>
+
+                <li>
+                    <a href="/accounting" class="<?php if($this->highlightMenu('accounting')) echo 'selected'; ?>">
+                        <i class="fa fa-money"></i>
+                    </a>
+                    <span><?= $text_ctrl_accounting; ?></span>
+                </li>
+
+                <li>
+                    <a href="/report" class="<?php if($this->highlightMenu('report')) echo 'selected'; ?>">
+                        <i class="fa fa-bar-chart"></i>
+                    </a>
+                    <span><?= $text_ctrl_stats; ?></span>
+                </li>
+
+                <li>
+                    <a href="/employee" class="<?php if($this->highlightMenu('employee')) echo 'selected'; ?>">
+                        <i class="fa fa-user"></i>
+                    </a>
+                    <span><?= $text_ctrl_employees; ?></span>
+                </li>
+
+                <li>
+                    <a href="/mail" class="<?php if($this->highlightMenu('mail')) echo 'selected'; ?>">
+                        <i class="fa fa-envelope"></i>
+                    </a>
+                    <span><?= $text_ctrl_mail; ?></span>
+                </li>
+
+                <li>
+                    <a href="/notification" class="<?php if($this->highlightMenu('notification')) echo 'selected'; ?>">
+                        <i class="fa fa-bell"></i>
+                    </a>
+                    <span><?= $text_ctrl_notifications; ?></span>
+                </li>
             </ul>
 		</div>
 	</div>
--- app/views/index/default.view.php
+++ app/views/index/default.view.php
@@ -15,29 +15,12 @@
         </header>
         <div class="contentBox clearfix">
             <ul class="dashboard_controls">
-                <li><a href="/moneyin" class="<?php if($this->highlightMenu('moneyin')) echo 'selected'; ?>"><i class="material-icons">move_to_inbox</i><?= $text_ctrl_money_in; ?></a></li>
-                <li><a href="/moneyout" class="<?php if($this->highlightMenu('moneyout')) echo 'selected'; ?>"><i class="fa fa-gift"></i><?= $text_ctrl_money_out; ?></a></li>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/volunteers" class="<?php if($this->highlightMenu('volunteers')) echo 'selected'; ?>"><i class="material-icons">group</i><?= $text_ctrl_volunteers; ?></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/client" class="<?php if($this->highlightMenu('client')) echo 'selected'; ?>"><i class="material-icons">contacts</i><?= $text_ctrl_clients; ?></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/store" class="<?php if($this->highlightMenu('store')) echo 'selected'; ?>"><i class="material-icons">store</i><?= $text_ctrl_store; ?></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/fillstore" class="<?php if($this->highlightMenu('fillstore')) echo 'selected'; ?>"><i class="material-icons">store</i><i class="fa fa-arrow-down" style="font-size: 100%"></i><?= $text_ctrl_fill_store; ?></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1))) { ?>
-                    <li><a href="/employee" class="<?php if($this->highlightMenu('employee')) echo 'selected'; ?>"><i class="fa fa-user"></i><?= $text_ctrl_employees; ?></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1,2))) { ?>
-                    <li><a href="/expenses" class="<?php if($this->highlightMenu('expenses')) echo 'selected'; ?>"><i class="fa fa-money"></i><?= $text_ctrl_expenses; ?></a></li>
-                <?php } ?>
-                <?php if($this->enableMenu($userPrivilege, array(1))) { ?>
-                    <li><a href="/report" class="<?php if($this->highlightMenu('report')) echo 'selected'; ?>"><i class="fa fa-bar-chart"></i><?= $text_ctrl_stats ?></a></li>
-                <?php } ?>
+                <li><a href="/suppliers" class="<?php if($this->highlightMenu('suppliers')) echo 'selected'; ?>"><i class="material-icons">group</i><?= $text_ctrl_suppliers; ?></a></li>
+                <li><a href="/client" class="<?php if($this->highlightMenu('client')) echo 'selected'; ?>"><i class="material-icons">contacts</i><?= $text_ctrl_clients; ?></a></li>
+                <li><a href="/store" class="<?php if($this->highlightMenu('store')) echo 'selected'; ?>"><i class="material-icons">store</i><?= $text_ctrl_store; ?></a></li>
+                <li><a href="/accounting" class="<?php if($this->highlightMenu('expenses')) echo 'selected'; ?>"><i class="fa fa-money"></i><?= $text_ctrl_accounting; ?></a></li>
+                <li><a href="/report" class="<?php if($this->highlightMenu('report')) echo 'selected'; ?>"><i class="fa fa-bar-chart"></i><?= $text_ctrl_stats ?></a></li>
+                <li><a href="/employee" class="<?php if($this->highlightMenu('employee')) echo 'selected'; ?>"><i class="fa fa-user"></i><?= $text_ctrl_employees; ?></a></li>
                 <li><a href="/mail" class="<?php if($this->highlightMenu('mail')) echo 'selected'; ?>"><i class="fa fa-envelope"></i><?= $text_ctrl_mail ?> (<?= $this->_registry->startup->mailTotal; ?>)</a></li>
                 <li><a href="/notification" class="<?php if($this->highlightMenu('notification')) echo 'selected'; ?>"><i class="fa fa-bell"></i><?= $text_ctrl_notifications ?> (<?= ($this->_registry->startup->notificationsTotal === false) ? 0 : count($this->_registry->startup->notificationsTotal) ?>)</a></li>
             </ul>
